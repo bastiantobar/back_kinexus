5296131e0f3d21ac621f99c82ef4a1a46e43e1e2
Bastian <bastiantobar.94@gmail.com>
2025-03-16 16:55:53 +0100

    first commit

---
+++ src/main/java/com/kinexus/back/BackinexusApplication.java
@@ -0,0 +1,13 @@
+package com.kinexus.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackinexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackinexusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kinexus/back/BackinexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kinexus.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackinexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
